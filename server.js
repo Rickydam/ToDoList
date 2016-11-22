c93363fa0de0798aa871396a018379b1e26ff220
Ricky Dam <ricky.dam10@hotmail.com>
2016-11-21 23:23:55 -0500

    Fixed typos

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.use(express.static(_dirname));
+app.use(express.static(__dirname));
 
 app.get("/", function(req, res) {
   res.render("index");
 });
 
-app.listen(port, function function() {
+app.listen(port, function() {
   console.log("App running");
 });
