96beb2902614949083872879eb46c4af7455c409
Ricky Dam <ricky.dam10@hotmail.com>
2016-11-29 22:04:50 -0500

    Enter key to add and no longer allows empty inputs

--- input.js
+++ input.js
@@ -1,6 +1,12 @@
 // Angular
 var app = angular.module('theToDoListApp', []);
 
+function checkEnter(e) {
+  if(e.keyCode==10 || e.keyCode==13) {
+    angular.element(document.getElementById("theDiv")).scope().createTodo();
+  }
+}
+
 function myController($scope, $http) {
   $scope.formData = {};
 
@@ -14,15 +20,22 @@ function myController($scope, $http) {
     });
 
   $scope.createTodo = function() {
-    $http.post('/items', $scope.formData)
-      .success(function(data) {
-        $scope.items = data;
-        $scope.formData = "";
-        console.log(data);
-      })
-      .error(function(data) {
-        console.log('Error: ' + data);
-      });
+    var input = document.getElementById("theTextField");
+    if(input.value == "") {
+      input.focus();
+    }
+    else {
+      $http.post('/items', $scope.formData)
+        .success(function(data) {
+          $scope.items = data;
+          $scope.formData = "";
+          console.log(data);
+          input.focus();
+        })
+        .error(function(data) {
+          console.log('Error: ' + data);
+        });
+    }
   };
 
   $scope.deleteTodo = function(id) {
