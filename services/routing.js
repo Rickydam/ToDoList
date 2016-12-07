0bcc41e35c60fcde6b9fb4d698c10cf0993447c3
Ricky Dam <ricky.dam10@hotmail.com>
2016-12-07 00:44:20 -0500

    Added route to my custom-made MobileView

--- services/routing.js
+++ services/routing.js
@@ -1,10 +1,27 @@
-var app = angular.module('theToDoListApp', ['ngRoute', 'controllers', 'ui.bootstrap']);
+var isMobile = false;
+function checkIfMobile() {
+  if(navigator.userAgent.match(/Android/i)
+  || navigator.userAgent.match(/webOS/i)
+  || navigator.userAgent.match(/iPhone/i)
+  || navigator.userAgent.match(/iPad/i)
+  || navigator.userAgent.match(/iPod/i)
+  || navigator.userAgent.match(/BlackBerry/i)
+  || navigator.userAgent.match(/Windows Phone/i)) {
+    isMobile =  true;
+  }
+  else {
+    isMobile = false;
+  }
+}
+checkIfMobile();
 
+var app = angular.module('theToDoListApp', ['ngRoute', 'controllers', 'ui.bootstrap']);
 app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'views/main.html',
-    controller: 'myController'
-  })
+  $routeProvider
+    .when('/', {
+      templateUrl: (isMobile)? 'views/mobile.html':'views/main.html',
+      controller: (isMobile)? 'myController':'myController'
+    })
 }]);
 
 // Remove the hashtag from localhost:8080/#
